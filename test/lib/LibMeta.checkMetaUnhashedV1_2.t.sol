5df70f149b13138c99c418cc761cca6fbe22bcf0
thedavidmeister <thedavidmeister@gmail.com>
2025-03-29 20:26:39 +0400

    fix tests

--- test/lib/LibMeta.checkMetaUnhashedV1_2.t.sol
+++ test/lib/LibMeta.checkMetaUnhashedV1_2.t.sol
@@ -7,6 +7,10 @@ import {LibMeta} from "src/lib/LibMeta.sol";
 import {UnexpectedMetaHash, NotRainMetaV1, META_MAGIC_NUMBER_V1} from "src/interface/unstable/IMetaV1_2.sol";
 
 contract LibMetaCheckMetaUnhashedV1_2Test is Test {
+    function checkMetaUnhashedV1External(bytes memory meta) external pure {
+        LibMeta.checkMetaUnhashedV1(meta);
+    }
+
     /// All data with the magic number prefix will be considered to be rain meta
     /// and all without will not. This test is the same as the above but with
     /// the revert due to the check.
@@ -15,6 +19,6 @@ contract LibMetaCheckMetaUnhashedV1_2Test is Test {
         LibMeta.checkMetaUnhashedV1(meta);
 
         vm.expectRevert(abi.encodeWithSelector(NotRainMetaV1.selector, data));
-        LibMeta.checkMetaUnhashedV1(data);
+        this.checkMetaUnhashedV1External(data);
     }
 }
