e5ed8a34d674cddd007e3fd28f262571a7079144
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-07-03 19:17:02 +0530

    fix test

--- subgraph/tests/utils.ts
+++ subgraph/tests/utils.ts
@@ -13,8 +13,8 @@ export function createNewMetaV1Event(sender: string, subject: Bytes, meta: Bytes
   // Set up transaction data
   metaV1Event.transaction.hash = Bytes.fromHexString(transactionHash);
   metaV1Event.transaction.from = Address.fromString(sender);
-  metaV1Event.block.number = BigInt.fromString(Math.floor(transactionBlockNumber).toString());
-  metaV1Event.block.timestamp = BigInt.fromString(Math.floor(transactionTimestamp).toString());
+  metaV1Event.block.number = BigInt.fromI32(transactionBlockNumber);
+  metaV1Event.block.timestamp = BigInt.fromI32(transactionTimestamp);
 
   let senderParam = new ethereum.EventParam("sender", ethereum.Value.fromAddress(Address.fromString(sender)));
   let subjectParam = new ethereum.EventParam("subject", ethereum.Value.fromBytes(subject));
